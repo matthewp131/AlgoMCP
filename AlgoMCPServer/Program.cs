272200db8117e4ef88757104a2498698d1e0dd41
matthewp131 <13476383+matthewp131@users.noreply.github.com>
2025-05-14 18:18:30 +0800

    Add project files.

---
+++ AlgoMCPServer/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// load secrets
+builder.Configuration.AddUserSecrets<Program>();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Register the background service
+builder.Services.AddHostedService<BackgroundTaskService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
