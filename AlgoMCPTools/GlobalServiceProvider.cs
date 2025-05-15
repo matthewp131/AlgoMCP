4dee6ad6bf0cd16076e2878b7dc21bc6c7c859c4
matthewp131 <13476383+matthewp131@users.noreply.github.com>
2025-05-15 19:46:53 +0800

    cleanup

--- AlgoMCPTools/GlobalServiceProvider.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace AlgoMCPTools
-{
-    public static class GlobalServiceProvider
-    {
-        public static IServiceProvider? ServiceProvider { get; set; }
-    }
-}
--- AlgoMCPTools/Program.cs
+++ AlgoMCPTools/Program.cs
@@ -29,10 +29,3 @@ builder.Services
     .WithToolsFromAssembly();
 
 await builder.Build().RunAsync();
-
-[McpServerToolType]
-public static class EchoTool
-{
-    [McpServerTool, Description("Echoes the message back to the client.")]
-    public static string Echo(string message) => $"hello {message}";
-}
